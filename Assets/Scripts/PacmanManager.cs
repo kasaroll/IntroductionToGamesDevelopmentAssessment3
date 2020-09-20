8e17eb1a8ae6d06767931410b60f7194fce5ae2d
KasraMosahebiMacBookPro <13539541@student.uts.edu.au>
2020-09-21 08:43:15 +1000

    Pac-man manager created

---
+++ Assets/Scripts/PacmanManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>().isPlaying == true && GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>().loop == false)
+        {
+            gameObject.GetComponent<Animator>().enabled = false;
+            gameObject.GetComponent<AudioSource>().Stop();
+        }
+        else
+        {
+            gameObject.GetComponent<Animator>().enabled = true;
+            if (!gameObject.GetComponent<AudioSource>().isPlaying)
+            {
+                gameObject.GetComponent<AudioSource>().Play();
+            }
+        }
+    }
+}

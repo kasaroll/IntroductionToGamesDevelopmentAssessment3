b709dc85a8533b3e58307e47789e09dbb77d2231
KasraMosahebiMacBookPro <13539541@student.uts.edu.au>
2020-09-21 10:11:19 +1000

    Final version

--- Assets/Scripts/PacmanManager.cs
+++ Assets/Scripts/PacmanManager.cs
@@ -7,8 +7,9 @@ public class PacmanManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
+
+    
 
     // Update is called once per frame
     void Update()
@@ -21,10 +22,46 @@ public class PacmanManager : MonoBehaviour
         else
         {
             gameObject.GetComponent<Animator>().enabled = true;
+
             if (!gameObject.GetComponent<AudioSource>().isPlaying)
             {
                 gameObject.GetComponent<AudioSource>().Play();
             }
+
+            if (gameObject.transform.position == new Vector3(-12.5f, 13.0f, 0.0f))
+            {
+                gameObject.transform.Rotate(0.0f, 0.0f, 45.0f);
+                StartCoroutine(LerpPosition(new Vector3(-7.5f, 13.0f, 0.0f), 1.0f));
+            }
+            else if (gameObject.transform.position == new Vector3(-7.5f, 13.0f, 0.0f))
+            {
+                gameObject.transform.Rotate(0.0f, 0.0f, 45.0f);
+                StartCoroutine(LerpPosition(new Vector3(-7.5f, 9.0f, 0.0f), 0.66f));
+            }
+            else if (gameObject.transform.position == new Vector3(-7.5f, 9.0f, 0.0f))
+            {
+                gameObject.transform.Rotate(0.0f, 0.0f, 45.0f);
+                StartCoroutine(LerpPosition(new Vector3(-12.5f, 9.0f, 0.0f), 1.0f));
+            }
+            else if (gameObject.transform.position == new Vector3(-12.5f, 9.0f, 0.0f))
+            {
+                gameObject.transform.Rotate(0.0f, 0.0f, 45.0f);
+                StartCoroutine(LerpPosition(new Vector3(-12.5f, 13.0f, 0.0f), 0.66f));
+            }
         }
+    }
+
+    IEnumerator LerpPosition(Vector2 targetPosition, float duration)
+    {
+        float time = 0;
+        Vector2 startPosition = gameObject.transform.position;
+
+        while (time < duration)
+        {
+            transform.position = Vector2.Lerp(startPosition, targetPosition, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPosition;
     }
 }

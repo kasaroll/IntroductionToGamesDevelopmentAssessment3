3290f0724ea497bb660fac9c6305af4793e248c5
KasraMosahebiMacBookPro <13539541@student.uts.edu.au>
2020-09-21 10:31:26 +1000

    Added power pellet animation

---
+++ Assets/Scripts/PowerPelletManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerPelletManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>().isPlaying == true && GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>().loop == false)
+        {
+            gameObject.GetComponent<Animator>().enabled = false;
+        } else
+        {
+            gameObject.GetComponent<Animator>().enabled = true;
+        }
+    }
+}
